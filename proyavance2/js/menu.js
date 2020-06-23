3fdbdfed71e1cc0dc7b98ec318e7c214539cad90
KAZZU307 <aureliavallejos0@gmail.com>
2020-06-23 08:34:44 -0400

    commit

---
+++ proyavance2/js/menu.js
@@ -0,0 +1,5 @@
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btmenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');
+})
---
+++ proyecto/js/jsfinal.js
@@ -0,0 +1,4 @@
+/*alert ('noo entre a esta pagIna ')*/
+function MenuDesplegable() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
