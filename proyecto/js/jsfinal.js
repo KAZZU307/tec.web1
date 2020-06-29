5cad427931aa7e10a45cea7858ec0c9752b551c3
KAZZU307 <aureliavallejos0@gmail.com>
2020-06-28 21:06:45 -0400

    commit

--- proyecto/js/jsfinal.js
+++ proyecto/js/jsfinal.js
@@ -1,4 +1,4 @@
 /*alert ('noo entre a esta pagIna ')*/
 function MenuDesplegable() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+	document.getElementsByClassName("navegacion-menucabecera")[0].classList.toggle("responsive");
 }
